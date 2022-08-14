59802a63f675007cc76c366edb7705eff62a1f84
Christopher Patrick <cpatrick67135@gmail.com>
2022-08-13 21:30:50 -0500

    optimized check win + added comments

--- index.js
+++ index.js
@@ -1,33 +1,94 @@
-x = true;
+x = true; //true when it's the human's turn
 p1Array = []; //will store human moves
 p2Array = []; //will store bot moves
 let p2Move = 0; //declares the variable for the bots move
-let humMatched = 0;
 allSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9]; //available squares
 let alphaAvail = 0;
 let values = [0, 0, 0, 0, 0, 0, 0]; //minimax will modify this
 let gameOver = false;
 
-winningNums = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-  [1, 4, 7],
-  [2, 5, 8],
-  [3, 6, 9],
-  [1, 5, 9],
-  [3, 5, 7],
-];
+var idToCoord = {
+  1: [0, 0],
+  2: [0, 1],
+  3: [0, 2],
+  4: [1, 0],
+  5: [1, 1],
+  6: [1, 2],
+  7: [2, 0],
+  8: [2, 1],
+  9: [2, 2],
+}
+
+//game starts when human clicks a square
+function HumanMove(square) {
+  if (!gameOver) {
+    //only make moves when the game isn't over
+    if (allSquares.includes(parseInt(square.id))) {
+      //no taking invalid squares!
+      removeElement(allSquares, parseInt(square.id));
+      x ? (square.innerHTML = "x") : (square.innerHTML = "o");
+      p1Array.push(parseInt(square.id));
+      // winDataStructureUpdate(square.id);
+      BotMove();
+    }
+    CheckTie();
+  }
+}
+
+function winDataStructureUpdate(id){
+  var coord = idToCoord[id];
+
+  //update the col
+  cols[coord[1]] += (x ? 1 : -1);
+  //update the row
+  rows[coord[0]] += (x ? 1 : -1);
+  //update the diag(s)
+  if (coord[0] == coord[1]){
+    //top left to bottom right
+    diagonal1 += (x ? 1 : -1);
+  }
+  if(coord[0]+coord[1] == 2){
+    diagonal2 += (x ? 1 : -1);
+  }
+}
+
+function BotMove() {
+  CheckTie();
+  if (!gameOver) {
+    alphaAvail = allSquares.length;
+    values = new Array(allSquares.length);
+    minimax(allSquares, p2Array, p1Array, allSquares.length, true); //minimax will change the values array to output each path's value
+    
+    //could later change p2Move to not be first best move only to vary gameplay
+    p2Move = allSquares[values.indexOf(Math.max(...values))]; //the first occurence of the highest value is the best move
+
+    if (allSquares.includes(p2Move)) {
+      removeElement(allSquares, p2Move);
+      x
+        ? (document.getElementById(p2Move.toString()).innerHTML = "o")
+        : (document.getElementById(p2Move.toString()).innerHTML = "x");
+      p2Array.push(p2Move);
+
+      if (CheckWin(p2Array, true)) {
+        gameOver = true;
+        document.getElementById("curPlayer").innerHTML = "Shiba Wins";
+      }
+    } else {
+      BotMove();
+    }
+  }
+}
+
 
 function minimax(availables, p2, p1, depth, isMaximizing) {
-  if (CheckWin(p2)) {
-    return 10 * (depth + 1);
-  } else if (CheckWin(p1)) {
+  if (CheckWin(p2, false)) {
+    return 10 * (depth + 1); // "* (depth + 1)" gives higher value to quick wins. if removed shiba will still win
+  } else if (CheckWin(p1, false)) {
     return -10;
   } else if (depth === 0) {
     return 0;
   } else {
-    if (isMaximizing) {
+    if (isMaximizing) { //isMaximizing=true means minimax is looking for shiba's best move (it's shiba's turn in tree)
       maxEval = -Infinity;
       for (let i = 0; i < availables.length; i++) {
         maxEval = Math.max(
@@ -70,78 +131,63 @@ function minimax(availables, p2, p1, depth, isMaximizing) {
 }
 
 function CheckWin(array, forColor) {
-  humMatched = 0;
-  let toBeChanged = []; //array will contain ids of the boxes that will be changed
-  for (let i = 0; i < winningNums.length; i++) {
-    for (let j = 0; j < array.length; j++) {
-      for (let k = 0; k < winningNums[i].length; k++) {
-        if (array[j] === winningNums[i][k]) {
-          humMatched++;
-          toBeChanged.push(array[j]);
-        }
-        if (humMatched == 3) {
-          if (forColor) {//When Checkwin is not used in minimax, it will change Shiba's winning squares
-            ChangeColor(toBeChanged, "green");
-            return true;
-          } else {
-            return true;
-          }
-        }
-      }
+  //winning states - one value will be three when a player wins
+  rows = [0, 0, 0]
+  cols = [0, 0, 0]
+  diagonal1 = 0
+  diagonal2 = 0
+
+  for(let i = 0; i < array.length; i++){
+    var coord = idToCoord[array[i].toString()];
+    //update the col
+    cols[coord[1]] += 1;
+    //update the row
+    rows[coord[0]] += 1;
+    //update the diag(s)
+    if (coord[0] == coord[1]){
+      //top left to bottom right
+      diagonal1 += 1;
     }
-    toBeChanged = [];
-    humMatched = 0;
+    if(coord[0]+coord[1] == 2){
+      diagonal2 += 1;
+    }
   }
 
-  humMatched = 0;
-  return false;
+  for (let i = 0; i < cols.length; i++){
+    if (cols[i] == 3){
+      if(forColor){
+        ChangeColor([i+1, i+4, i+7], "green")
+      }
+      return true;
+    }
+  }
+  for (let i = 0; i < rows.length; i++){
+    if (rows[i] == 3){
+      if(forColor){
+        ChangeColor([1+(i*3), 2+(i*3), 3+(i*3)], "green")
+      }
+      return true;
+    }
+  }
+  if (diagonal1 == 3){
+    if(forColor){
+      ChangeColor([1, 5, 9], "green")
+    }
+    return true;
+  }
+  if (diagonal2 == 3){
+    if(forColor){
+      ChangeColor([3, 5, 7], "green")
+    }
+    return true;
+  }
+  return false
 }
 
 function removeElement(array, elem) {//used for removing moves from allSquared
   let index = array.indexOf(elem);
   if (index > -1) {
     array.splice(index, 1);
-  }
-}
-
-function BotMove() {
-  CheckTie();
-  if (!gameOver) {
-    alphaAvail = allSquares.length;
-    values = new Array(allSquares.length);
-    minimax(allSquares, p2Array, p1Array, allSquares.length, true); //minimax will change the values array to output each path's value
-
-    p2Move = allSquares[values.indexOf(Math.max(...values))]; //the first occurence of the highest value is the best move
-
-    if (allSquares.includes(p2Move)) {
-      removeElement(allSquares, p2Move);
-      x
-        ? (document.getElementById(p2Move.toString()).innerHTML = "o")
-        : (document.getElementById(p2Move.toString()).innerHTML = "x");
-      p2Array.push(p2Move);
-
-      if (CheckWin(p2Array, true)) {
-        gameOver = true;
-        document.getElementById("curPlayer").innerHTML = "Shiba Wins";
-      }
-    } else {
-      BotMove();
-    }
-  }
-}
-
-function HumanMove(yo) {
-  if (!gameOver) {
-    //only make moves when the game isn't over
-    if (allSquares.includes(parseInt(yo.id))) {
-      //no taking invalid squares!
-      removeElement(allSquares, parseInt(yo.id));
-      x ? (yo.innerHTML = "x") : (yo.innerHTML = "o");
-      p1Array.push(parseInt(yo.id));
-
-      BotMove();
-    }
-    CheckTie();
   }
 }
 
